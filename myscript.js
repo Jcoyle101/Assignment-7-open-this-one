5395f761882bcd0ca3b0d0cd8d77fbcdc734e617
Jcoyle101 <jackcoyle23@yahoo.com>
2022-11-19 23:42:19 -0500

    Assignment 7
    Assignment 7. Open this one. Cyberpunk shop.

---
+++ myscript.js
@@ -0,0 +1,13 @@
+onload="myFunction()"
+
+function Display() {
+   alert("Order Submitted. Thank you!")
+}
+
+function Display2() {
+   alert("Details submitted. Thank you!")
+}
+
+
+Time = document.lastModified;
+
